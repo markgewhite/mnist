7cc0b57b8f9566b7285e5dd2893289e49953a0a6
markgewhite <markgewhite@gmail.com>
2026-01-17 15:15:56 +0000

    Weakened the discriminator by removing conv4

--- src/models/discriminator.py
+++ src/models/discriminator.py
@@ -11,16 +11,18 @@ WEIGHT_INIT = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
 
 class Discriminator(BaseNetwork):
-    """Discriminator matching MATLAB exactly:
+    """Discriminator - reduced capacity version.
 
-    MATLAB:
+    Removed conv4 (224 filters) to weaken discriminator.
+    Original was overpowering the generator.
+
+    Architecture:
         imageInputLayer([28,28,1], Normalization='none')
         dropoutLayer(0.5)
-        conv2dLayer(5, 28, stride=2, same)  -> lrelu1
-        conv2dLayer(5, 56, stride=2, same)  -> bn2 -> lrelu2
-        conv2dLayer(5, 112, stride=2, same) -> bn3 -> lrelu3
-        conv2dLayer(5, 224, stride=2, same) -> bn4 -> lrelu4
-        conv2dLayer(2, 1)
+        conv2dLayer(5, 28, stride=2, same)  -> lrelu1        [output: 14x14x28]
+        conv2dLayer(5, 56, stride=2, same)  -> bn2 -> lrelu2 [output: 7x7x56]
+        conv2dLayer(5, 112, stride=2, same) -> bn3 -> lrelu3 [output: 4x4x112]
+        conv2dLayer(4, 1)                                    [output: 1x1x1]
     """
 
     def __init__(self, dropout_rate: float = 0.5, leaky_alpha: float = 0.2, name: str = "Discriminator"):
@@ -35,18 +37,14 @@ class Discriminator(BaseNetwork):
 
         # conv2dLayer(5, 56, stride=2, same) -> bn2
         self.conv2 = layers.Conv2D(56, 5, strides=2, padding="same", kernel_initializer=WEIGHT_INIT, name="conv2")
-        self.bn2 = layers.BatchNormalization(momentum=0.9, name="bn2")  # Match MATLAB
+        self.bn2 = layers.BatchNormalization(momentum=0.9, name="bn2")
 
         # conv2dLayer(5, 112, stride=2, same) -> bn3
         self.conv3 = layers.Conv2D(112, 5, strides=2, padding="same", kernel_initializer=WEIGHT_INIT, name="conv3")
-        self.bn3 = layers.BatchNormalization(momentum=0.9, name="bn3")  # Match MATLAB
+        self.bn3 = layers.BatchNormalization(momentum=0.9, name="bn3")
 
-        # conv2dLayer(5, 224, stride=2, same) -> bn4
-        self.conv4 = layers.Conv2D(224, 5, strides=2, padding="same", kernel_initializer=WEIGHT_INIT, name="conv4")
-        self.bn4 = layers.BatchNormalization(momentum=0.9, name="bn4")  # Match MATLAB
-
-        # conv2dLayer(2, 1) - final logit
-        self.conv5 = layers.Conv2D(1, 2, strides=1, padding="valid", kernel_initializer=WEIGHT_INIT, name="conv5")
+        # conv2dLayer(4, 1) - final logit (4x4 kernel to go from 4x4 to 1x1)
+        self.conv4 = layers.Conv2D(1, 4, strides=1, padding="valid", kernel_initializer=WEIGHT_INIT, name="conv4")
 
     def build_layers(self):
         pass  # Layers built in __init__
@@ -69,13 +67,8 @@ class Discriminator(BaseNetwork):
         x = self.bn3(x, training=training)
         x = tf.nn.leaky_relu(x, alpha=self.leaky_alpha)
 
-        # conv4 -> bn4 -> lrelu4
+        # conv4 -> logit (4x4 kernel, no activation)
         x = self.conv4(x)
-        x = self.bn4(x, training=training)
-        x = tf.nn.leaky_relu(x, alpha=self.leaky_alpha)
-
-        # conv5 -> logit
-        x = self.conv5(x)
         x = tf.reshape(x, [-1, 1])
 
         return x
